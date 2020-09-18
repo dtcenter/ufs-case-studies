96d306c2c2eac2d1df5f0b0c840e516b87951fae
Xia Sun <xia.sun@noaa.gov>
2020-09-18 16:06:49 -0600

    update conf.py and clean ups

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -114,4 +114,5 @@ epub_title = project
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+nbsphinx_allow_errors = True
 html_static_path = ['_static']
